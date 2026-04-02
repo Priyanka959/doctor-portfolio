edde57c7eeb54d2669e22ab6c8c4ac6afcfeda05
Priyanka Kulal <priyankakulal1205@gmail.com>
2026-04-02 11:19:06 +0530

    last

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactCompiler: true,
+  experimental: {
+    allowedDevOrigins: ['192.168.0.105'],
+  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
